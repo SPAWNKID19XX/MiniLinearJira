dfaa4794b4ece70fc2fe6d224d54d72211a8680b
spawnkid1987 <borisisac@gmail.com>
2026-09-20 01:28:18 +0100

    [FIX] users password hashing on create and update

--- miniLeniarJira/apps/users/serializers.py
+++ miniLeniarJira/apps/users/serializers.py
@@ -6,3 +6,24 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ["first_name", "last_name", "email", "password"]
+    extra_kwargs= {
+      "password":{
+        "write_only": True
+      }
+    }
+
+  def create(self, validated_data):
+    return User.objects.create_user(**validated_data)
+
+  def update(self, instance, validated_data):
+
+    pass_date = validated_data.get("password", None)
+
+    instance = super().update(instance, validated_data)
+
+    if pass_date:
+      instance.set_password(pass_date)
+      instance.save()
+
+    return instance
+      
